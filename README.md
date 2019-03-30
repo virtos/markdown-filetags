3e2bc564054ac6dce58fa20f7291884589b2849e
virtos <evirt752@hotmail.com>
2019-03-30 20:59:48 +0300

    ver 0.9

--- README.md
+++ README.md
@@ -5,9 +5,14 @@ Extracts file tags (+tag in the filename) into markdown files
 - [ ] write a hint output
 - [x] get current dir from the command line
 - [ ] check if the specified directory exists
-- [ ] list of all .md files in the directory
-- [ ] list all the tags to the console
-- [ ] rewrite inde file
-- [ ] list of all files in subdirs
-- [ ] rewrite index files for subdirs
+- [x] list of all .md files in the directory
+- [x] list all the tags to the console
+- [x] rewrite inde file
+- [x] list of all files in subdirs
+- [x] rewrite index files for subdirs
+- [x] bug: only one file is created for each tag
 - [ ] detect if rewriting is needed
+- [x] compile for x86 (GOOS=windows GOARCH=amd64 go build -o filetags.exe main.go)
+- [ ] use tables for presentation
+- [ ] replace underscores with spaces
+- [ ] sort file names
--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 )
 
-const tagSymbol string = "-"
+const tagSymbol string = "+"
+const indexFileName string = "_filetags.md"
 
 var whiteSpaceDelim = [...]string{" ", "_"}
+
+type tf struct {
+	Tag  string
+	File []string
+}
 
 func check(err error) {
 	if err != nil {
@@ -24,42 +30,79 @@ func main() {
 	if len(args) > 1 {
 		targetDir = args[1]
 	}
-
-	targetDir, err := filepath.Abs(filepath.Dir(targetDir))
+	targetDir, err := filepath.Abs(targetDir)
 	check(err)
 
 	processDir(targetDir)
 }
 
 func processDir(dir string) {
-	fmt.Println("Processing directory ", dir, "..")
 	lst, err := ioutil.ReadDir(dir)
 	check(err)
-	tags := make(map[string]string)
+
+	tfs := make([]tf, 0)
 
 	for _, file := range lst {
 
 		if file.IsDir() {
 			defer processDir(filepath.Join(dir, file.Name()))
-			// } else if filepath.Ext(strings.TrimSpace(file.Name())) != ".md" {
-			// 	continue
+			continue
+		} else if filepath.Ext(strings.TrimSpace(file.Name())) != ".md" {
+			continue
 		} else if !strings.Contains(file.Name(), tagSymbol) {
 			continue
 		}
-
-		arr := strings.Split(file.Name(), tagSymbol)
-		for _, elem := range arr {
-			if string(elem[1]) == file.Name() {
-				break
+		fileName := strings.ToLower(file.Name())
+		arr := strings.Split(fileName, tagSymbol)
+		for i, elem := range arr {
+			if elem == "" {
+				continue
+			}
+			if i == 0 && fileName[:1] != tagSymbol {
+				continue
 			}
 			// take the first value before any delimiter
 			elem += " "
 			curTag := elem[:strings.IndexAny(elem, " _.")]
-			//tags[file.Name()] = curTag
-			tags[curTag] = file.Name()
-		}
 
-		fmt.Println("File :", file.Name())
-		fmt.Println(tags)
+			//record found tag and filename
+			found := false
+			for i := range tfs {
+				if tfs[i].Tag == curTag {
+					tfs[i].File = append(tfs[i].File, file.Name())
+					found = true
+					break
+				}
+			}
+			if !found {
+				tfs = append(tfs, tf{curTag, []string{fileName}})
+			}
+		}
+	}
+
+	// sort tags
+	sort.Slice(tfs, func(i, j int) bool {
+		return tfs[i].Tag < tfs[j].Tag
+	})
+
+	// result
+	if len(tfs) > 0 {
+		f, err := os.Create(filepath.Join(dir, indexFileName))
+		check(err)
+		defer f.Close()
+		_, err = f.WriteString("---\nheading:accordion\n...\n\n")
+		check(err)
+
+		for _, ctf := range tfs {
+			_, err := f.WriteString("### " + ctf.Tag + "\n|   |\n|---|\n")
+			check(err)
+			sort.Slice(ctf.File, func(i, j int) bool {
+				return ctf.File[i] < ctf.File[j]
+			})
+			for _, cf := range ctf.File {
+				_, err := f.WriteString("| [" + strings.Replace(cf, "_", " ", -1) + "](" + strings.Replace(cf, " ", "%20", -1) + ") |\n")
+				check(err)
+			}
+		}
 	}
 }
